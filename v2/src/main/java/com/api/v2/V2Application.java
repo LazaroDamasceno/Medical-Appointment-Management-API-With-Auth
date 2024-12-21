d491af892d5496efb1c35c8d67ce92023bb1e2f4
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-21 14:02:30 -0300

    Enabled async

--- v2/src/main/java/com/api/v2/V2Application.java
+++ v2/src/main/java/com/api/v2/V2Application.java
@@ -3,8 +3,10 @@ package com.api.v2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class V2Application {
 
 	public static void main(String[] args) {
