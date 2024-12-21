3e9eff9b69224837ace1899ce6077c950d1170c1
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-21 16:43:54 -0300

    Created the event publisher CustomerModificationEventPublisher

---
+++ v2/src/main/java/com/api/v2/customers/events/CustomerModificationEventPublisher.java
@@ -0,0 +1,38 @@
+package com.api.v2.customers.events;
+
+import com.api.v2.customers.dtos.CustomerModificationDto;
+import com.api.v2.customers.dtos.CustomerRegistrationDto;
+import com.api.v2.customers.dtos.CustomerResponseDto;
+import com.api.v2.customers.services.CustomerModificationService;
+import com.api.v2.customers.services.CustomerRegistrationService;
+import com.api.v2.customers.utils.CustomerFinderUtil;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomerModificationEventPublisher {
+
+    private final ApplicationEventPublisher eventPublisher;
+    private final CustomerModificationService modificationService;
+    private final CustomerFinderUtil customerFinderUtil;
+
+    public CustomerModificationEventPublisher(
+            ApplicationEventPublisher eventPublisher,
+            CustomerModificationService modificationService,
+            CustomerFinderUtil customerFinderUtil
+    ) {
+        this.eventPublisher = eventPublisher;
+        this.modificationService = modificationService;
+        this.customerFinderUtil = customerFinderUtil;
+    }
+
+    public Mono<Void> publish(String ssn, CustomerModificationDto modificationDto) {
+        return modificationService.modify(ssn, modificationDto)
+                .zipWith(customerFinderUtil.findBySsn(ssn))
+                .flatMap(tuple -> {
+                    eventPublisher.publishEvent(tuple.getT2());
+                    return Mono.empty();
+                });
+    }
+}
