6f18813d0637a6678b7c3344ebcf1d47dd944c78
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-21 16:50:23 -0300

    Changed the controller CustomerController

--- v2/src/main/java/com/api/v2/customers/controllers/CustomerController.java
+++ v2/src/main/java/com/api/v2/customers/controllers/CustomerController.java
@@ -3,9 +3,8 @@ package com.api.v2.customers.controllers;
 import com.api.v2.customers.dtos.CustomerModificationDto;
 import com.api.v2.customers.dtos.CustomerRegistrationDto;
 import com.api.v2.customers.dtos.CustomerResponseDto;
+import com.api.v2.customers.events.CustomerModificationEventPublisher;
 import com.api.v2.customers.events.CustomerRegistrationEventPublisher;
-import com.api.v2.customers.services.CustomerModificationService;
-import com.api.v2.customers.services.CustomerRegistrationService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -15,24 +14,23 @@ import reactor.core.publisher.Mono;
 public class CustomerController {
 
     private final CustomerRegistrationEventPublisher registrationEventPublisher;
-    //private final CustomerModificationService modificationService;
+    private final CustomerModificationEventPublisher modificationEventPublisher;
 
     public CustomerController(
-            CustomerRegistrationEventPublisher registrationEventPublisher
-            //CustomerModificationService modificationService
+            CustomerRegistrationEventPublisher registrationEventPublisher,
+            CustomerModificationEventPublisher modificationEventPublisher
     ) {
         this.registrationEventPublisher = registrationEventPublisher;
-        //this.modificationService = modificationService;
+        this.modificationEventPublisher = modificationEventPublisher;
     }
-
 
     @PostMapping
     public Mono<CustomerResponseDto> register(@Valid @RequestBody CustomerRegistrationDto registrationDto) {
         return registrationEventPublisher.publish(registrationDto);
     }
 
-//    @PatchMapping("{ssn}")
-//    public Mono<Void> modify(@PathVariable String ssn, @Valid @RequestBody CustomerModificationDto modificationDto) {
-//        return modificationService.modify(ssn, modificationDto);
-//    }
+    @PatchMapping("{ssn}")
+    public Mono<Void> modify(@PathVariable String ssn, @Valid @RequestBody CustomerModificationDto modificationDto) {
+        return modificationEventPublisher.publish(ssn, modificationDto);
+    }
 }
