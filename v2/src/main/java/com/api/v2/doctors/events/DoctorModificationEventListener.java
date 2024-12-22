a695026694ceac77c9eff6cf5937b5ee6f935c69
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-22 00:56:34 -0300

    Created the event listener DoctorModificationEventListener

---
+++ v2/src/main/java/com/api/v2/doctors/events/DoctorModificationEventListener.java
@@ -0,0 +1,21 @@
+package com.api.v2.doctors.events;
+
+import com.api.v2.doctors.services.DoctorModificationService;
+import com.api.v2.people.dtos.PersonModificationDto;
+import jakarta.validation.Valid;
+import org.springframework.modulith.events.ApplicationModuleListener;
+import reactor.core.publisher.Mono;
+
+public class DoctorModificationEventListener {
+
+    private final DoctorModificationService doctorModificationService;
+
+    public DoctorModificationEventListener(DoctorModificationService doctorModificationService) {
+        this.doctorModificationService = doctorModificationService;
+    }
+
+    @ApplicationModuleListener
+    public Mono<Void> modify(String medicalLicenseNumber, @Valid PersonModificationDto modificationDto) {
+        return doctorModificationService.modify(medicalLicenseNumber, modificationDto);
+    }
+}
