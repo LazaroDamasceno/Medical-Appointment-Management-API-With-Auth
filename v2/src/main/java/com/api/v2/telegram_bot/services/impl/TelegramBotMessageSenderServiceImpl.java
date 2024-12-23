5b50c94659cf694e808b8721e0633bd0021aeba8
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-23 20:47:01 -0300

    Changed TelegramBotMessageSenderServiceImpl

--- v2/src/main/java/com/api/v2/telegram_bot/services/impl/TelegramBotMessageSenderServiceImpl.java
+++ v2/src/main/java/com/api/v2/telegram_bot/services/impl/TelegramBotMessageSenderServiceImpl.java
@@ -2,6 +2,7 @@ package com.api.v2.telegram_bot.services.impl;
 
 import com.api.v2.telegram_bot.bot.TelegramBot;
 import com.api.v2.telegram_bot.services.interfaces.TelegramBotMessageSenderService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -11,13 +12,15 @@ public class TelegramBotMessageSenderServiceImpl implements TelegramBotMessageSe
 
     private final TelegramBot telegramBot;
 
+    @Value("${bot.chatId}")
+    private String chatId;
+
     public TelegramBotMessageSenderServiceImpl(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
     }
 
     @Override
     public void sendMessage(String message) throws TelegramApiException {
-        telegramBot.execute(new SendMessage("209797404", message));
+        telegramBot.execute(new SendMessage(chatId, message));
     }
-
 }
