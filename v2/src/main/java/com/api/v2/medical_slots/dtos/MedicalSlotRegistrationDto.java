679b5223de0457c52e05ac93503d80f2151f3454
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-24 20:05:05 -0300

    Created the dto MedicalSlotRegistrationDto

---
+++ v2/src/main/java/com/api/v2/medical_slots/dtos/MedicalSlotRegistrationDto.java
@@ -0,0 +1,9 @@
+package com.api.v2.medical_slots.dtos;
+
+import java.time.LocalDateTime;
+
+public record MedicalSlotRegistrationDto(
+        String medicalLicenseNumber,
+        LocalDateTime availableAt
+) {
+}
