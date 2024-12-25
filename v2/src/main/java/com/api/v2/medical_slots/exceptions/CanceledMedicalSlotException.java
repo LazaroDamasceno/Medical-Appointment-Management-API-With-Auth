91fcd42f4122653359b9718757d3a2d2961ee21e
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-24 21:06:37 -0300

    Created the exception NonExistentMedicalAppointmentException

--- v2/src/main/java/com/api/v2/medical_slots/exceptions/CanceledMedicalSlotException.java
+++ v2/src/main/java/com/api/v2/medical_slots/exceptions/CanceledMedicalSlotException.java
@@ -3,7 +3,7 @@ package com.api.v2.medical_slots.exceptions;
 import com.api.v2.doctors.exceptions.ImmutableDoctorException;
 import org.bson.types.ObjectId;
 
-public class CanceledMedicalSlotException extends ImmutableDoctorException {
+public class CanceledMedicalSlotException extends RuntimeException {
     public CanceledMedicalSlotException(ObjectId id) {
         super("Medical slot whose id is %s is already canceled.".formatted(id));
     }
