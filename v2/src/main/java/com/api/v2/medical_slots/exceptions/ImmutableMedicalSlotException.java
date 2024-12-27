d06934fc3064e8d441cc2ee1310019373205ada7
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-27 02:15:04 -0300

    Created the exception ImmutableMedicalSlotException

---
+++ v2/src/main/java/com/api/v2/medical_slots/exceptions/ImmutableMedicalSlotException.java
@@ -0,0 +1,7 @@
+package com.api.v2.medical_slots.exceptions;
+
+public class ImmutableMedicalSlotException extends RuntimeException {
+    public ImmutableMedicalSlotException(String message) {
+        super(message);
+    }
+}
