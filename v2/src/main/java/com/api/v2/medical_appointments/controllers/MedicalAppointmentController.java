7191600f7c745614d5d86a374ec8b88bce2c08ac
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-03 04:29:29 -0300

    Changed MedicalAppointmentFinderUtil

--- v2/src/main/java/com/api/v2/medical_appointments/controllers/MedicalAppointmentController.java
+++ v2/src/main/java/com/api/v2/medical_appointments/controllers/MedicalAppointmentController.java
@@ -3,8 +3,6 @@ package com.api.v2.medical_appointments.controllers;
 import com.api.v2.medical_appointments.dtos.MedicalAppointmentBookingDto;
 import com.api.v2.medical_appointments.dtos.MedicalAppointmentResponseDto;
 import com.api.v2.medical_appointments.services.interfaces.MedicalAppointmentBookingService;
-import com.api.v2.medical_appointments.services.interfaces.MedicalAppointmentCancellationService;
-import com.api.v2.medical_appointments.services.interfaces.MedicalAppointmentCompletionService;
 import com.api.v2.medical_appointments.services.interfaces.MedicalAppointmentRetrievalService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
@@ -16,35 +14,19 @@ import reactor.core.publisher.Mono;
 public class MedicalAppointmentController {
 
     private final MedicalAppointmentBookingService bookingService;
-    private final MedicalAppointmentCancellationService cancellationService;
-    private final MedicalAppointmentCompletionService completionService;
     private final MedicalAppointmentRetrievalService retrievalService;
 
     public MedicalAppointmentController(
             MedicalAppointmentBookingService bookingService,
-            MedicalAppointmentCancellationService cancellationService,
-            MedicalAppointmentCompletionService completionService,
             MedicalAppointmentRetrievalService retrievalService
     ) {
         this.bookingService = bookingService;
-        this.cancellationService = cancellationService;
-        this.completionService = completionService;
         this.retrievalService = retrievalService;
     }
 
     @PostMapping
     public Mono<MedicalAppointmentResponseDto> book(@Valid @RequestBody MedicalAppointmentBookingDto bookingDto) {
         return bookingService.book(bookingDto);
-    }
-
-    @PatchMapping("{id}/cancellation")
-    public Mono<Void> cancel(@PathVariable String id) {
-        return cancellationService.cancel(id);
-    }
-
-    @PatchMapping("{id}/completion")
-    public Mono<Void> complete(@PathVariable String id) {
-        return completionService.complete(id);
     }
 
     @GetMapping("{id}")
--- v2/src/main/java/com/api/v2/medical_appointments/services/interfaces/MedicalAppointmentCancellationService.java
+++
@@ -1,7 +0,0 @@
-package com.api.v2.medical_appointments.services.interfaces;
-
-import reactor.core.publisher.Mono;
-
-public interface MedicalAppointmentCancellationService {
-    Mono<Void> cancel(String id);
-}
