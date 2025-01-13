a4cf78078d93e1075ae6a27d2d330d164eb2fa84
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-13 19:37:02 -0300

    Deleted the telegram bot

--- v2/src/main/java/com/api/v2/telegram_bot/services/interfaces/TelegramBotMessageSenderService.java
+++
@@ -1,7 +0,0 @@
-package com.api.v2.telegram_bot.services.interfaces;
-
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-public interface TelegramBotMessageSenderService {
-    void sendMessage(String message) throws TelegramApiException;
-}
