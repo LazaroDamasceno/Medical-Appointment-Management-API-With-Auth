0f34ae61f05c0fc9f2de3ff227a9ee73151ccab3
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-14 18:29:49 -0300

    Created the test class MedicalAppointmentBookingTestMedicalAppointmentBookingTest

---
+++ v2/src/test/java/com/api/v2/MedicalAppointmentBookingTest.java
@@ -0,0 +1,84 @@
+package com.api.v2;
+
+import com.api.v2.medical_appointments.dtos.MedicalAppointmentBookingDto;
+import com.api.v2.medical_appointments.dtos.MedicalAppointmentResponseDto;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MedicalAppointmentBookingTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private final MedicalAppointmentBookingDto bookingDto = new MedicalAppointmentBookingDto(
+            "123456789",
+            "12345678CA",
+            LocalDateTime.parse("2025-12-12T12:30:30")
+    );
+
+    @Order(1)
+    @Test
+    void testSuccessfulBooking() {
+        webTestClient
+                .post()
+                .uri("api/v2/medical-appointments")
+                .bodyValue(bookingDto)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(MedicalAppointmentResponseDto.class);
+    }
+
+    @Order(2)
+    @Test
+    void testUnsuccessfulBookingDueToDuplicatedBookingDateTime() {
+        webTestClient
+                .post()
+                .uri("api/v2/medical-appointments")
+                .bodyValue(bookingDto)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    private final MedicalAppointmentBookingDto bookingDto2 = new MedicalAppointmentBookingDto(
+            "123456788",
+            "12345678CA",
+            LocalDateTime.parse("2025-12-12T12:30:30")
+    );
+
+    @Order(2)
+    @Test
+    void testUnsuccessfulBookingDueToNotFoundCustomer() {
+        webTestClient
+                .post()
+                .uri("api/v2/medical-appointments")
+                .bodyValue(bookingDto2)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    private final MedicalAppointmentBookingDto bookingDto3 = new MedicalAppointmentBookingDto(
+            "123456789",
+            "12345677CA",
+            LocalDateTime.parse("2025-12-12T12:30:30")
+    );
+
+    @Order(2)
+    @Test
+    void testUnsuccessfulBookingDueToNotFoundDoctor() {
+        webTestClient
+                .post()
+                .uri("api/v2/medical-appointments")
+                .bodyValue(bookingDto3)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+}
