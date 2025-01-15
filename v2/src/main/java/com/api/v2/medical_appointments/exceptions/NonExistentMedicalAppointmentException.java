1348bce94b4b5ca2898f5bf06862e1eb0f7eff48
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-14 22:56:29 -0300

    Changed the message of MedicalAppointmentCompletionServiceImpl

--- v2/src/main/java/com/api/v2/medical_appointments/exceptions/NonExistentMedicalAppointmentException.java
+++ v2/src/main/java/com/api/v2/medical_appointments/exceptions/NonExistentMedicalAppointmentException.java
@@ -2,6 +2,6 @@ package com.api.v2.medical_appointments.exceptions;
 
 public class NonExistentMedicalAppointmentException extends RuntimeException {
     public NonExistentMedicalAppointmentException(String id) {
-        super("Medical appointment whose id id %s was not found.".formatted(id));
+        super("Medical appointment whose id is %s was not found.".formatted(id));
     }
 }
