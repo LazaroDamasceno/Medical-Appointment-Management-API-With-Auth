8a10b8d08c670366870394d500f9c7eb7dbd77db
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-16 13:52:35 -0300

    Transformed the record MedicalAppointmentResponseDto into a class

--- v2/src/main/java/com/api/v2/medical_appointments/dtos/MedicalAppointmentResponseDto.java
+++ v2/src/main/java/com/api/v2/medical_appointments/dtos/MedicalAppointmentResponseDto.java
@@ -1,21 +1,75 @@
 package com.api.v2.medical_appointments.dtos;
 
 import com.api.v2.customers.dtos.CustomerResponseDto;
+import com.api.v2.customers.utils.CustomerResponseMapper;
 import com.api.v2.doctors.dtos.DoctorResponseDto;
+import com.api.v2.doctors.utils.DoctorResponseMapper;
+import com.api.v2.medical_appointments.domain.MedicalAppointment;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public record MedicalAppointmentResponseDto(
-        String id,
-        CustomerResponseDto customerResponseDto,
-        DoctorResponseDto doctorResponseDto,
-        String type,
-        LocalDateTime bookedAt,
-        ZoneId bookAtZone,
-        LocalDateTime canceledAt,
-        ZoneId canceledAtZone,
-        LocalDateTime completedAt,
-        ZoneId completedAtZone
-) {
+public class MedicalAppointmentResponseDto {
+
+    private ObjectId id;
+    private CustomerResponseDto customerResponseDto;
+    private DoctorResponseDto doctorResponseDto;
+    private String type;
+    private LocalDateTime bookedAt;
+    private ZoneId bookAtZone;
+
+    public MedicalAppointmentResponseDto() {
+    }
+
+    protected MedicalAppointmentResponseDto(
+            ObjectId id,
+            CustomerResponseDto customerResponseDto,
+            DoctorResponseDto doctorResponseDto,
+            String type,
+            LocalDateTime bookedAt,
+            ZoneId bookAtZone
+    ) {
+        this.id = id;
+        this.customerResponseDto = customerResponseDto;
+        this.doctorResponseDto = doctorResponseDto;
+        this.type = type;
+        this.bookedAt = bookedAt;
+        this.bookAtZone = bookAtZone;
+    }
+
+    public static MedicalAppointmentResponseDto create(MedicalAppointment medicalAppointment) {
+        return new MedicalAppointmentResponseDto(
+                medicalAppointment.getId(),
+                CustomerResponseMapper.mapToDto(medicalAppointment.getCustomer()),
+                DoctorResponseMapper.mapToDto(medicalAppointment.getDoctor()),
+                medicalAppointment.getType(),
+                medicalAppointment.getBookedAt(),
+                medicalAppointment.getBookedAtZone()
+        );
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public CustomerResponseDto getCustomerResponseDto() {
+        return customerResponseDto;
+    }
+
+    public DoctorResponseDto getDoctorResponseDto() {
+        return doctorResponseDto;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDateTime getBookedAt() {
+        return bookedAt;
+    }
+
+    public ZoneId getBookAtZone() {
+        return bookAtZone;
+    }
 }
