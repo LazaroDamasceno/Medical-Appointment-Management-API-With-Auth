b45ac2262bc9e0e6a6916de34a71d2566f09a03d
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-16 18:13:48 -0300

    Created the dto MedicalSlotWithMedicalAppointmentResponseDto

---
+++ v2/src/main/java/com/api/v2/medical_slots/dtos/MedicalSlotWithMedicalAppointmentResponseDto.java
@@ -0,0 +1,29 @@
+package com.api.v2.medical_slots.dtos;
+
+import com.api.v2.medical_appointments.dtos.MedicalAppointmentResponseDto;
+import com.api.v2.medical_appointments.utils.MedicalAppointmentResponseMapper;
+import com.api.v2.medical_slots.domain.MedicalSlot;
+
+public class MedicalSlotWithMedicalAppointmentResponseDto extends MedicalSlotResponseDto {
+
+    private MedicalAppointmentResponseDto medicalAppointmentResponseDto;
+
+    public MedicalSlotWithMedicalAppointmentResponseDto() {
+    }
+
+    private MedicalSlotWithMedicalAppointmentResponseDto(MedicalSlot medicalSlot) {
+        super(medicalSlot);
+        if (medicalSlot.getMedicalAppointment() == null) {
+            return;
+        }
+        this.medicalAppointmentResponseDto = MedicalAppointmentResponseMapper.mapToDto(medicalSlot.getMedicalAppointment());
+    }
+
+    public static MedicalSlotWithMedicalAppointmentResponseDto create(MedicalSlot medicalSlot) {
+        return new MedicalSlotWithMedicalAppointmentResponseDto(medicalSlot);
+    }
+
+    public MedicalAppointmentResponseDto getMedicalAppointmentResponseDto() {
+        return medicalAppointmentResponseDto;
+    }
+}
