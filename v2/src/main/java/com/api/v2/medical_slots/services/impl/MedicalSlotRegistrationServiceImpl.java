919d02efd3a532b7b5050e348a44a57d6906b13c
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-19 00:39:25 -0300

    Removed one unsed import from NonExistentCardException

--- v2/src/main/java/com/api/v2/medical_slots/services/impl/MedicalSlotRegistrationServiceImpl.java
+++ v2/src/main/java/com/api/v2/medical_slots/services/impl/MedicalSlotRegistrationServiceImpl.java
@@ -46,7 +46,6 @@ public class MedicalSlotRegistrationServiceImpl implements MedicalSlotRegistrati
                             .then(Mono.defer(() -> {
                                 MedicalSlot medicalSlot = MedicalSlot.create(doctor, registrationDto.availableAt());
                                 String id = medicalSlot.getId().toString();
-                                String medicalLicenseNumber = medicalSlot.getDoctor().getLicenseNumber();
                                 return medicalSlotRepository
                                         .save(medicalSlot)
                                         .then(Mono.defer(() -> {
