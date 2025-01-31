e9ffe2f6c239dacfe99dc454aa62fbc4fe8691d8
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-31 02:36:11 -0300

    Removed unused imports from MedicalSlotResponseDto

--- v2/src/main/java/com/api/v2/medical_slots/dtos/MedicalSlotResponseDto.java
+++ v2/src/main/java/com/api/v2/medical_slots/dtos/MedicalSlotResponseDto.java
@@ -1,9 +1,7 @@
 package com.api.v2.medical_slots.dtos;
 
 import com.api.v2.doctors.dtos.DoctorResponseDto;
-import com.api.v2.doctors.utils.DoctorResponseMapper;
 import com.api.v2.medical_appointments.dtos.MedicalAppointmentResponseDto;
-import com.api.v2.medical_slots.domain.MedicalSlot;
 
 public record MedicalSlotResponseDto(
         String id,
