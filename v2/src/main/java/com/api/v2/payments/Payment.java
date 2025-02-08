e4d95bff8018ac3272c7b727e9b30faa36e1b2e5
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-08 20:25:36 -0300

    Created the entity Payment

---
+++ v2/src/main/java/com/api/v2/payments/Payment.java
@@ -0,0 +1,35 @@
+package com.api.v2.payments;
+
+import com.api.v2.cards.domain.Card;
+import com.api.v2.medical_appointments.domain.MedicalAppointment;
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Service
+public record Payment(
+        @BsonId
+        ObjectId id,
+        Card card,
+        MedicalAppointment medicalAppointment,
+        LocalDateTime payedAt,
+        ZoneId payedAtZoneId,
+        ZoneOffset payedAtZoneOffset
+) {
+
+    public static Payment create(Card card, MedicalAppointment medicalAppointment) {
+        return new Payment(
+                new ObjectId(),
+                card,
+                medicalAppointment,
+                LocalDateTime.now(),
+                ZoneId.systemDefault(),
+                OffsetTime.now().getOffset()
+        );
+    }
+}
