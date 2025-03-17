2048bf9962446e9c037e576a4d08ec68d9d9bbec
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-17 04:49:36 -0300

    Fixed MedicalAppointmentResponseDto

--- v2/src/main/java/com/api/v2/medical_appointments/dtos/MedicalAppointmentResponseDto.java
+++ v2/src/main/java/com/api/v2/medical_appointments/dtos/MedicalAppointmentResponseDto.java
@@ -2,12 +2,13 @@ package com.api.v2.medical_appointments.dtos;
 
 import com.api.v2.customers.dtos.CustomerResponseDto;
 import com.api.v2.doctors.dtos.DoctorResponseDto;
+import com.api.v2.medical_appointments.MedicalAppointmentType;
 
 public record MedicalAppointmentResponseDto(
         String id,
         CustomerResponseDto customer,
         DoctorResponseDto doctor,
-        String type,
+        MedicalAppointmentType type,
         String bookedAt,
         String canceledAt,
         String completedAt
