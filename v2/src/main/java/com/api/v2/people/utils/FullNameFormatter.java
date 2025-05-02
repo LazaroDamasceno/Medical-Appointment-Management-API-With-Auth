2d3b465c53dbaae0aff81924c4dda39510cfcb71
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 03:21:24 -0300

    Created the utility class FullNameFormatter

---
+++ v2/src/main/java/com/api/v2/people/utils/FullNameFormatter.java
@@ -0,0 +1,16 @@
+package com.api.v2.people.utils;
+
+import com.api.v2.people.domain.exposed.Person;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FullNameFormatter {
+
+    public String format(Person person) {
+        if (person.getMiddleName() == null) {
+            return String.format("%s %s", person.getFirstName(), person.getLastName());
+        }
+        return String.format("%s %s %s", person.getFirstName(), person.getMiddleName(), person.getLastName());
+    }
+
+}
