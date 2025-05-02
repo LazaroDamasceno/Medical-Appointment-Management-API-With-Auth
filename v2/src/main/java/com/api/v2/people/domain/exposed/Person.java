ed3e66138f711665e8a0ea2a920d621ee3dd5200
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 03:49:53 -0300

    Turned the entity Person from a class to a record

--- v2/src/main/java/com/api/v2/people/domain/exposed/Person.java
+++ v2/src/main/java/com/api/v2/people/domain/exposed/Person.java
@@ -2,8 +2,6 @@ package com.api.v2.people.domain.exposed;
 
 import com.api.v2.people.enums.Gender;
 import com.api.v2.people.requests.PersonRegistrationDto;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,37 +10,33 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document
-@Getter
-@NoArgsConstructor
-public class Person {
-
+public record Person (
     @Id
-    private String id;
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String ssn;
-    private LocalDate birthDate;
-    private String phoneNumber;
-    private Gender gender;
-    private String email;
-    private LocalDateTime createdAt;
-
-    private Person(PersonRegistrationDto registrationDto) {
-        this.id = UUID.randomUUID().toString();
-        this.firstName = registrationDto.firstName();
-        this.middleName = registrationDto.middleName();
-        this.lastName = registrationDto.lastName();
-        this.ssn = registrationDto.ssn();
-        this.birthDate = registrationDto.birthDate();
-        this.phoneNumber = registrationDto.phoneNumber();
-        this.gender = registrationDto.gender();
-        this.email = registrationDto.email();
-        this.createdAt = LocalDateTime.now();
-    }
+    String id,
+    String firstName,
+    String middleName,
+    String lastName,
+    String ssn,
+    LocalDate birthDate,
+    String phoneNumber,
+    Gender gender,
+    String email,
+    LocalDateTime createdAt
+) {
 
     public static Person of(PersonRegistrationDto registrationDto) {
-        return new Person(registrationDto);
+        return new Person(
+                UUID.randomUUID().toString(),
+                registrationDto.firstName(),
+                registrationDto.middleName(),
+                registrationDto.lastName(),
+                registrationDto.ssn(),
+                registrationDto.birthDate(),
+                registrationDto.phoneNumber(),
+                registrationDto.gender(),
+                registrationDto.email(),
+                LocalDateTime.now()
+        );
     }
 
 }
