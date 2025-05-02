24dfc1e4743a0caa8136e11aa5134bb988903ed4
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 02:52:20 -0300

    Added the query find to PersonRepository

--- v2/src/main/java/com/api/v2/people/domain/PersonRepository.java
+++ v2/src/main/java/com/api/v2/people/domain/PersonRepository.java
@@ -1,6 +1,12 @@
 package com.api.v2.people.domain;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface PersonRepository extends ReactiveMongoRepository<Person, String> {
+
+    @Query("{ 'ssn': ?0, 'email': ?1 }")
+    Mono<Person> find(String ssn, String email);
+
 }
