3bba3583c248f7571f986743244332a1b3f15aea
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 20:38:07 -0300

    Created the entity PersonAuditTrail

---
+++ v2/src/main/java/com/api/v2/people/domain/PersonAuditTrail.java
@@ -0,0 +1,25 @@
+package com.api.v2.people.domain;
+
+import com.api.v2.people.domain.exposed.Person;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document
+public record PersonAuditTrail(
+        @Id
+        String id,
+        Person person,
+        LocalDateTime createdAt
+) {
+
+    public static PersonAuditTrail of(Person person) {
+        return new PersonAuditTrail(
+                UUID.randomUUID().toString(),
+                person,
+                LocalDateTime.now()
+        );
+    }
+}
