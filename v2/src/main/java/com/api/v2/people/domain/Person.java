a874b9f94488e822f55b52add9a3898ecddca744
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 02:53:42 -0300

    Added a private constructor and a static factory method of to Person

--- v2/src/main/java/com/api/v2/people/domain/Person.java
+++ v2/src/main/java/com/api/v2/people/domain/Person.java
@@ -1,6 +1,7 @@
 package com.api.v2.people.domain;
 
 import com.api.v2.people.enums.Gender;
+import com.api.v2.people.requests.PersonRegistrationDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Document
 @Getter
@@ -23,6 +25,24 @@ public class Person {
     private LocalDate birthDate;
     private String phoneNumber;
     private Gender gender;
+    private String email;
     private LocalDateTime createdAt;
+
+    private Person(PersonRegistrationDto registrationDto) {
+        this.id = UUID.randomUUID().toString();
+        this.firstName = registrationDto.firstName();
+        this.middleName = registrationDto.middleName();
+        this.lastName = registrationDto.lastName();
+        this.ssn = registrationDto.ssn();
+        this.birthDate = registrationDto.birthDate();
+        this.phoneNumber = registrationDto.phoneNumber();
+        this.gender = registrationDto.gender();
+        this.email = registrationDto.email();
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static Person of(PersonRegistrationDto registrationDto) {
+        return new Person(registrationDto);
+    }
 
 }
