572ee31db3acef334ce30c1edd5fdaadf6a985d1
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 02:58:31 -0300

    Created the service PersonRegistrationService

---
+++ v2/src/main/java/com/api/v2/people/services/PersonRegistrationServiceImpl.java
@@ -0,0 +1,29 @@
+package com.api.v2.people.services;
+
+import com.api.v2.people.domain.Person;
+import com.api.v2.people.domain.PersonRepository;
+import com.api.v2.people.requests.PersonRegistrationDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public abstract class PersonRegistrationServiceImpl implements PersonRegistrationService {
+
+    private final PersonRepository personRepository;
+
+    @Override
+    public Mono<Person> register(PersonRegistrationDto registrationDto) {
+        return personRepository
+                .find(registrationDto.ssn(), registrationDto.email())
+                .singleOptional()
+                .flatMap(optional -> {
+                    if (optional.isPresent()) {
+                        return Mono.just(optional.get());
+                    }
+                    Person person = Person.of(registrationDto);
+                    return personRepository.save(person);
+                });
+    }
+}
