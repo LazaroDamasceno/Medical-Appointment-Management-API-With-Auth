faa6886fb9e7a9f1dca23729f1d2f6ce40d37b56
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 17:36:52 -0300

    Created the entity Doctor

---
+++ v2/src/main/java/com/api/v2/doctors/Doctor.java
@@ -0,0 +1,30 @@
+package com.api.v2.doctors;
+
+import com.api.v2.people.domain.exposed.Person;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Doctor(
+        @Id
+        String id,
+        MedicalLicenseNumber medicalLicenseNumber,
+        Person person,
+        LocalDateTime createdAAt,
+        DoctorStatus status,
+        LocalDateTime modifiedAt
+) {
+
+    public static Doctor of(MedicalLicenseNumber medicalLicenseNumber, Person person) {
+        return new Doctor(
+                UUID.randomUUID().toString(),
+                medicalLicenseNumber,
+                person,
+                LocalDateTime.now(),
+                DoctorStatus.ACTIVE,
+                null
+        );
+    }
+
+}
