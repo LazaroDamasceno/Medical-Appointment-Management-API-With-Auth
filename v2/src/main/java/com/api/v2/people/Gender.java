24c48e7c377dfb8363616a7ddcfe78144c2883d8
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 02:25:03 -0300

    Created the enum Gender

---
+++ v2/src/main/java/com/api/v2/people/Gender.java
@@ -0,0 +1,14 @@
+package com.api.v2.people;
+
+public enum Gender {
+
+    CIS_MALE,
+    CIS_FEMALE,
+    TRANS_MALE,
+    TRANS_FEMALE,
+    QUEER,
+    NON_BINARY,
+    OTHER,
+    NOT_INFORMED
+
+}
