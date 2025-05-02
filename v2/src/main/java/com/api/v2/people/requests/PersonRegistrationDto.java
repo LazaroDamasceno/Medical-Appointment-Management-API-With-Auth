ce0c356a0e80c3d12fbe7a5898b7ae14f1d72c86
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 02:34:35 -0300

    Created the DTO PersonRegistrationDto

---
+++ v2/src/main/java/com/api/v2/people/requests/PersonRegistrationDto.java
@@ -0,0 +1,27 @@
+package com.api.v2.people.requests;
+
+import com.api.v2.people.enums.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record PersonRegistrationDto(
+        @NotBlank
+        String firstName,
+        String middleName,
+        @NotBlank
+        String lastName,
+        @NotBlank
+        @Size(min = 9, max = 9)
+        String ssn,
+        @NotNull
+        LocalDate birthDate,
+        @NotBlank
+        @Size(min = 10, max = 10)
+        String phoneNumber,
+        @NotNull
+        Gender gender
+) {
+}
