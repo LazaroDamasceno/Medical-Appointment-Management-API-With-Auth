86c967c7eb3d1c78b240f0abecc572eb2a05cf33
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-02 22:08:48 -0300

    Changed the controller CustomerController

--- v2/src/main/java/com/api/v2/customers/controllers/CustomerController.java
+++ v2/src/main/java/com/api/v2/customers/controllers/CustomerController.java
@@ -2,14 +2,13 @@ package com.api.v2.customers.controllers;
 
 import com.api.v2.customers.responses.CustomerResponseDto;
 import com.api.v2.customers.services.CustomerRegistrationService;
+import com.api.v2.customers.services.CustomerUpdatingService;
 import com.api.v2.people.requests.PersonRegistrationDto;
+import com.api.v2.people.requests.PersonUpdatingDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -18,9 +17,15 @@ import reactor.core.publisher.Mono;
 public class CustomerController {
 
     private final CustomerRegistrationService registrationService;
+    private final CustomerUpdatingService updatingService;
 
     @PostMapping
     public Mono<ResponseEntity<CustomerResponseDto>> register(@Valid @RequestBody PersonRegistrationDto registrationDto) {
         return registrationService.register(registrationDto);
+    }
+
+    @PatchMapping("{customerId}")
+    public Mono<Void> update(@PathVariable String customerId, @Valid @RequestBody PersonUpdatingDto updatingDto) {
+        return updatingService.update(customerId, updatingDto);
     }
 }
