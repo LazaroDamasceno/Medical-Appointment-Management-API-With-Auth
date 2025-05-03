ae202519c8eb0656d44c6b085fff3ab5493fda87
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-03 02:32:14 -0300

    Turned the entity Person from record to class

--- v1/src/main/java/com/api/v1/people/domain/exposed/Person.java
+++ v1/src/main/java/com/api/v1/people/domain/exposed/Person.java
@@ -4,6 +4,8 @@ import com.api.v1.people.dtos.Address;
 import com.api.v1.people.enums.Gender;
 import com.api.v1.people.requests.PersonRegistrationDto;
 import com.api.v1.people.requests.PersonUpdatingDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,24 +14,49 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document
-public record Person (
+@Getter
+@NoArgsConstructor
+public final class Person {
+
     @Id
-    String id,
-    String firstName,
-    String middleName,
-    String lastName,
-    String ssn,
-    LocalDate birthDate,
-    String phoneNumber,
-    Gender gender,
-    String email,
-    Address address,
-    LocalDateTime createdAt
-) {
+    private String id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String ssn;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private Gender gender;
+    private String email;
+    private Address address;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private Person(String firstName,
+                  String middleName,
+                  String lastName,
+                  String ssn,
+                  LocalDate birthDate,
+                  String phoneNumber,
+                  Gender gender,
+                  String email,
+                  Address address
+    ) {
+        this.id = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.email = email;
+        this.address = address;
+        this.createdAt = LocalDateTime.now();
+    }
 
     public static Person of(PersonRegistrationDto registrationDto) {
         return new Person(
-                UUID.randomUUID().toString(),
                 registrationDto.firstName(),
                 registrationDto.middleName(),
                 registrationDto.lastName(),
@@ -38,25 +65,19 @@ public record Person (
                 registrationDto.phoneNumber(),
                 registrationDto.gender(),
                 registrationDto.email(),
-                registrationDto.address(),
-                LocalDateTime.now()
+                registrationDto.address()
         );
     }
 
-    public static Person of(PersonUpdatingDto updatingDto, String ssn) {
-        return new Person(
-                UUID.randomUUID().toString(),
-                updatingDto.firstName(),
-                updatingDto.middleName(),
-                updatingDto.lastName(),
-                ssn,
-                updatingDto.birthDate(),
-                updatingDto.phoneNumber(),
-                updatingDto.gender(),
-                updatingDto.email(),
-                updatingDto.address(),
-                LocalDateTime.now()
-        );
+    public void update(PersonUpdatingDto updatingDto) {
+        this.firstName = updatingDto.firstName();
+        this.middleName = updatingDto.middleName();
+        this.lastName = updatingDto.lastName();
+        this.birthDate = updatingDto.birthDate();
+        this.phoneNumber = updatingDto.phoneNumber();
+        this.gender = updatingDto.gender();
+        this.email = updatingDto.email();
+        this.address = updatingDto.address();
     }
 
 }
