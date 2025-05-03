aed2a58cabd4bd144dff92a48bf061e29d810b77
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-03 04:03:13 -0300

    Enabled Hypermedia Support and HATEOAS

--- v1/src/main/java/com/api/v1/V1Application.java
+++ v1/src/main/java/com/api/v1/V1Application.java
@@ -2,9 +2,11 @@ package com.api.v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = {})
 public class V1Application {
 
 	public static void main(String[] args) {
