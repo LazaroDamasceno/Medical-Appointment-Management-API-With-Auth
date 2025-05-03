fcefebcd870717ad57fb021530b6f513ff6f0dad
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-03 17:01:08 -0300

    Changed CustomerController

--- v1/src/main/java/com/api/v1/customers/controllers/CustomerController.java
+++ v1/src/main/java/com/api/v1/customers/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import com.api.v1.customers.services.CustomerRegistrationService;
 import com.api.v1.customers.services.CustomerUpdatingService;
 import com.api.v1.people.requests.PersonRegistrationDto;
 import com.api.v1.people.requests.PersonUpdatingDto;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +21,13 @@ public class CustomerController {
     private final CustomerUpdatingService updatingService;
 
     @PostMapping
+    @Operation(summary = "Register a new customer")
     public Mono<ResponseEntity<CustomerResponseDto>> register(@Valid @RequestBody PersonRegistrationDto registrationDto) {
         return registrationService.register(registrationDto);
     }
 
     @PatchMapping("{customerId}")
+    @Operation(summary = "Update a customer")
     public Mono<ResponseEntity<Void>> update(@PathVariable String customerId, @Valid @RequestBody PersonUpdatingDto updatingDto) {
         return updatingService.update(customerId, updatingDto);
     }
