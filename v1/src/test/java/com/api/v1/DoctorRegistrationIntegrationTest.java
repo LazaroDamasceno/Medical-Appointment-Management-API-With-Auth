67ab15476572dce172863f72b9e7a7682dc51d16
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-03 00:45:02 -0300

    Fixed the testing class DoctorRegistrationIntegrationTest

--- v1/src/test/java/com/api/v1/DoctorRegistrationIntegrationTest.java
+++ v1/src/test/java/com/api/v1/DoctorRegistrationIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.api.v1;
 
 import com.api.v1.common.States;
+import com.api.v1.doctors.domain.MedicalLicenseNumber;
 import com.api.v1.doctors.requests.DoctorRegistrationDto;
 import com.api.v1.people.dtos.Address;
 import com.api.v1.people.enums.Gender;
@@ -23,7 +24,26 @@ class DoctorRegistrationIntegrationTest {
 	WebTestClient webTestClient;
 
 	DoctorRegistrationDto registrationDto = new DoctorRegistrationDto(
-
+		new PersonRegistrationDto(
+				"Christian",
+				"",
+				"Sewer",
+				"987654321",
+				LocalDate.of(2000,12,12),
+				"1234567890",
+				Gender.CIS_MALE,
+				"chrissewer@mail.com",
+				new Address(
+						"Downtown",
+						"Los Angeles",
+						States.CA,
+						"90012"
+				)
+		),
+		new MedicalLicenseNumber(
+				"12345678",
+				States.CA
+		)
 	);
 
 	@Test
@@ -31,27 +51,115 @@ class DoctorRegistrationIntegrationTest {
 	void testSuccessfulRegistration() {
 		webTestClient
 				.post()
-				.uri("api/v1/customers")
+				.uri("api/v1/doctors")
 				.bodyValue(registrationDto)
 				.exchange()
 				.expectStatus()
 				.is2xxSuccessful();
 	}
 
-	PersonRegistrationDto registrationDtoWithDuplicatedSSn = new PersonRegistrationDto(
-			"Leonard",
-			"",
-			"Smith",
-			"123456789",
-			LocalDate.of(2000,12,12),
-			"1234567890",
-			Gender.CIS_MALE,
-			"leosmith@mail.com",
-			new Address(
-					"Downtown",
-					"Los Angeles",
-					States.CA,
-					"90012"
+	DoctorRegistrationDto registrationDtoWithDuplicatedSSn = new DoctorRegistrationDto(
+			new PersonRegistrationDto(
+					"Christian",
+					"",
+					"Sewer",
+					"987654321",
+					LocalDate.of(2000,12,12),
+					"1234567890",
+					Gender.CIS_MALE,
+					"chrissewer@mail.com",
+					new Address(
+							"Downtown",
+							"Los Angeles",
+							States.CA,
+							"90012"
+					)
+			),
+			new MedicalLicenseNumber(
+					"12345678",
+					States.CA
 			)
 	);
+
+	@Test
+	@Order(2)
+	void testUnsuccessfulRegistration_Duplicated_Ssn() {
+		webTestClient
+				.post()
+				.uri("api/v1/doctors")
+				.bodyValue(registrationDtoWithDuplicatedSSn)
+				.exchange()
+				.expectStatus()
+				.is5xxServerError();
+	}
+
+	DoctorRegistrationDto registrationDtoWithDuplicatedEmail = new DoctorRegistrationDto(
+			new PersonRegistrationDto(
+					"Christian",
+					"",
+					"Sewer",
+					"234156789",
+					LocalDate.of(2000,12,12),
+					"1234567890",
+					Gender.CIS_MALE,
+					"chrissewer@mail.com",
+					new Address(
+							"Downtown",
+							"Los Angeles",
+							States.CA,
+							"90012"
+					)
+			),
+			new MedicalLicenseNumber(
+					"12345678",
+					States.CA
+			)
+	);
+
+	@Test
+	@Order(3)
+	void testUnsuccessfulRegistration_Duplicated_Email() {
+		webTestClient
+				.post()
+				.uri("api/v1/doctors")
+				.bodyValue(registrationDtoWithDuplicatedEmail)
+				.exchange()
+				.expectStatus()
+				.is5xxServerError();
+	}
+
+	DoctorRegistrationDto registrationDtoWithDuplicatedMedicalLicenseNumber = new DoctorRegistrationDto(
+			new PersonRegistrationDto(
+					"Christian",
+					"",
+					"Sewer",
+					"234156789",
+					LocalDate.of(2000,12,12),
+					"1234567890",
+					Gender.CIS_MALE,
+					"chrissewer@chrissewer.com",
+					new Address(
+							"Downtown",
+							"Los Angeles",
+							States.CA,
+							"90012"
+					)
+			),
+			new MedicalLicenseNumber(
+					"12345678",
+					States.CA
+			)
+	);
+
+	@Test
+	@Order(4)
+	void testUnsuccessfulRegistration_Duplicated_Medical_License_Number() {
+		webTestClient
+				.post()
+				.uri("api/v1/doctors")
+				.bodyValue(registrationDtoWithDuplicatedMedicalLicenseNumber)
+				.exchange()
+				.expectStatus()
+				.is5xxServerError();
+	}
 }
