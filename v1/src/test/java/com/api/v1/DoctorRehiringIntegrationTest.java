337f8e169fef0040de2167f03df993ea74218e9a
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-03 16:46:42 -0300

    Created the testing classes DoctorRehiringIntegrationTest and DoctorTerminationIntegrationTest.

---
+++ v1/src/test/java/com/api/v1/DoctorRehiringIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.api.v1;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DoctorRehiringIntegrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testSuccessfulRehiring() {
+        String id  = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s/rehiring".formatted(id ))
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    void testUnsuccessfulRehiring_ActiveDoctor() {
+        String id  = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s/rehiring".formatted(id ))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
---
+++ v1/src/test/java/com/api/v1/DoctorTerminationIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.api.v1;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DoctorTerminationIntegrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testSuccessfulTermination() {
+        String id  = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s/termination".formatted(id ))
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    void testUnsuccessfulTermination_TerminatedDoctor() {
+        String id  = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s/termination".formatted(id ))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
