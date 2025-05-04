2dbbc3bdcf6ff8d933ea4768dbcc5ca4793841a6
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-04 02:40:32 -0300

    Created the entity MedicalSlot and its repository

---
+++ v1/src/main/java/com/api/v1/medical_slots/domain/MedicalSlot.java
@@ -0,0 +1,49 @@
+package com.api.v1.medical_slots.domain;
+
+import com.api.v1.doctors.domain.exposed.Doctor;
+import com.api.v1.medical_slots.MedicalSlotStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document
+@Getter
+@NoArgsConstructor
+public class MedicalSlot {
+
+    @Id
+    private String id;
+    private Doctor doctor;
+    private LocalDateTime availableAt;
+    private LocalDateTime createdAt;
+    private MedicalSlotStatus status;
+    private LocalDateTime canceledAt;
+    private LocalDateTime completedAt;
+
+    private MedicalSlot(Doctor doctor, LocalDateTime availableAt) {
+        this.id = UUID.randomUUID().toString();
+        this.doctor = doctor;
+        this.availableAt = availableAt;
+        this.createdAt = LocalDateTime.now();
+        this.status = MedicalSlotStatus.ACTIVE;
+    }
+
+    public static MedicalSlot of(Doctor doctor, LocalDateTime availableAt) {
+        return new MedicalSlot(doctor, availableAt);
+    }
+
+    public void markAsCanceled() {
+        status = MedicalSlotStatus.CANCELED;
+        canceledAt = LocalDateTime.now();
+    }
+
+    public void markAsCompleted() {
+        status = MedicalSlotStatus.COMPLETED;
+        completedAt = LocalDateTime.now();
+    }
+
+}
---
+++ v1/src/main/java/com/api/v1/medical_slots/domain/MedicalSlotRepository.java
@@ -0,0 +1,14 @@
+package com.api.v1.medical_slots.domain;
+
+import com.api.v1.doctors.domain.exposed.Doctor;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+public interface MedicalSlotRepository extends ReactiveMongoRepository<MedicalSlot, String> {
+
+    @Query("{ 'doctor':?0, 'availableAt':?1 }")
+    Mono<MedicalSlot> find(Doctor doctor, LocalDateTime availableAt);
+}
