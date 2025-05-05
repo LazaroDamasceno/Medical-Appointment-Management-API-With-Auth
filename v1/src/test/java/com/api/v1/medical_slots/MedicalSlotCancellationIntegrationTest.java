b536581687818323610175543f116eeb96d0a09c
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-05 02:04:55 -0300

    Created the testing class MedicalSlotCancellationIntegrationTest

---
+++ v1/src/test/java/com/api/v1/medical_slots/MedicalSlotCancellationIntegrationTest.java
@@ -0,0 +1,98 @@
+package com.api.v1.medical_slots;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MedicalSlotCancellationIntegrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Order(1)
+    @Test
+    void testSuccessfulCancellation() {
+        String doctorId = "";
+        String medicalSlotId = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-slots/%s/%s/cancellation".formatted(doctorId, medicalSlotId))
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Order(2)
+    @Test
+    void testUnsuccessfulCancellation_DoctorNotFound() {
+        String doctorId = UUID.randomUUID().toString();
+        String medicalSlotId = UUID.randomUUID().toString();
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-slots/%s/%s/cancellation".formatted(doctorId, medicalSlotId))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+    @Order(3)
+    @Test
+    void testUnsuccessfulCancellation_MedicalSlotNotFound() {
+        String doctorId = UUID.randomUUID().toString();
+        String medicalSlotId = UUID.randomUUID().toString();
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-slots/%s/%s/cancellation".formatted(doctorId, medicalSlotId))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+    @Order(4)
+    @Test
+    void testUnsuccessfulCancellation_NotAssociatedDoctor() {
+        String doctorId = "";
+        String medicalSlotId = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-slots/%s/%s/cancellation".formatted(doctorId, medicalSlotId))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+    @Order(5)
+    @Test
+    void testUnsuccessfulCancellation_CanceledMedicalSlot() {
+        String doctorId = "";
+        String medicalSlotId = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-slots/%s/%s/cancellation".formatted(doctorId, medicalSlotId))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+    @Order(6)
+    @Test
+    void testUnsuccessfulCancellation_CompletedMedicalSlot() {
+        String doctorId = "";
+        String medicalSlotId = "";
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-slots/%s/%s/cancellation".formatted(doctorId, medicalSlotId))
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
--- v1/src/test/java/com/api/v1/medical_slots/MedicalSlotRegistrationIntegrationTest.java
+++ v1/src/test/java/com/api/v1/medical_slots/MedicalSlotRegistrationIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.api.v1;
+package com.api.v1.medical_slots;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
