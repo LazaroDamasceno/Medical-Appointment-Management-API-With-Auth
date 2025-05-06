c06e130ab7398b29cca1efe270766fc672f91763
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-06 02:34:12 -0300

    Created the exception NotActiveMedicalSlotException

---
+++ v1/src/main/java/com/api/v1/medical_slots/exceptions/NotActiveMedicalSlotException.java
@@ -0,0 +1,7 @@
+package com.api.v1.medical_slots.exceptions;
+
+public class NotActiveMedicalSlotException extends RuntimeException {
+    public NotActiveMedicalSlotException(String id) {
+      super("Medical slot whose id is %s is currently not active.".formatted(id));
+    }
+}
