3f76b9f4f45954695955dab38fce95b9279534c4
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-08 17:57:10 -0300

    Updated the exception NotActiveMedicalSlotException

--- v1/src/main/java/com/api/v1/medical_slots/exceptions/NotActiveMedicalSlotException.java
+++ v1/src/main/java/com/api/v1/medical_slots/exceptions/NotActiveMedicalSlotException.java
@@ -1,7 +1,12 @@
 package com.api.v1.medical_slots.exceptions;
 
 public class NotActiveMedicalSlotException extends RuntimeException {
+
     public NotActiveMedicalSlotException(String id) {
       super("Medical slot whose id is %s is currently not active.".formatted(id));
+    }
+
+    public NotActiveMedicalSlotException() {
+        super("Sought medical slot is not active.");
     }
 }
