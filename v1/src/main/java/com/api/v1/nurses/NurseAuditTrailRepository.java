4b4963d9d40c2dfa911b57ae532074f76c8427f5
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-09 02:54:56 -0300

    Created the repository NurseAuditTrailRepository

---
+++ v1/src/main/java/com/api/v1/nurses/NurseAuditTrailRepository.java
@@ -0,0 +1,6 @@
+package com.api.v1.nurses;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface NurseAuditTrailRepository extends ReactiveMongoRepository<NurseAuditTrail, String> {
+}
