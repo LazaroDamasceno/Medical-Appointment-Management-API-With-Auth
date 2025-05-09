a3503ee5732d494e674c4f57fd6a00256b9d366f
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-09 03:07:32 -0300

    Created the DTO NurseResponseDto

--- v1/src/main/java/com/api/v1/doctors/responses/DoctorResponseDto.java
+++ v1/src/main/java/com/api/v1/doctors/responses/DoctorResponseDto.java
@@ -1,5 +1,6 @@
 package com.api.v1.doctors.responses;
 
+import com.api.v1.common.ProfessionalStatus;
 import com.api.v1.doctors.dtos.MedicalLicenseNumber;
 import com.api.v1.doctors.domain.exposed.Doctor;
 import com.api.v1.people.utils.FullNameFormatter;
@@ -8,26 +9,34 @@ import org.springframework.hateoas.RepresentationModel;
 public class DoctorResponseDto extends RepresentationModel<DoctorResponseDto> {
 
     private String fullName;
+    private ProfessionalStatus status;
     private MedicalLicenseNumber licenseNumber;
 
     DoctorResponseDto() {}
 
     private DoctorResponseDto(String fullName,
+                              ProfessionalStatus status,
                               MedicalLicenseNumber medicalLicenseNumber
     ) {
         this.fullName = fullName;
+        this.status = status;
         this.licenseNumber = medicalLicenseNumber;
     }
 
     public static DoctorResponseDto from(Doctor doctor) {
         return new DoctorResponseDto(
                 FullNameFormatter.format(doctor.getPerson()),
+                doctor.getStatus(),
                 doctor.getLicenseNumber()
         );
     }
 
     public String getFullName() {
         return fullName;
+    }
+
+    public ProfessionalStatus getStatus() {
+        return status;
     }
 
     public MedicalLicenseNumber getLicenseNumber() {
---
+++ v1/src/main/java/com/api/v1/nurses/responses/NurseResponseDto.java
@@ -0,0 +1,45 @@
+package com.api.v1.nurses.responses;
+
+import com.api.v1.common.ProfessionalStatus;
+import com.api.v1.nurses.domain.exposed.Nurse;
+import com.api.v1.nurses.dtos.NurseLicenseNumber;
+import com.api.v1.people.utils.FullNameFormatter;
+import org.springframework.hateoas.RepresentationModel;
+
+public class NurseResponseDto extends RepresentationModel<NurseResponseDto> {
+
+    private String fullName;
+    private ProfessionalStatus status;
+    private NurseLicenseNumber licenseNumber;
+
+    private NurseResponseDto() {}
+
+    private NurseResponseDto(String fullName,
+                             ProfessionalStatus status,
+                             NurseLicenseNumber licenseNumber
+    ) {
+        this.fullName = fullName;
+        this.status = status;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public static NurseResponseDto from(Nurse nurse) {
+        return new NurseResponseDto(
+                FullNameFormatter.format(nurse.getPerson()),
+                nurse.getStatus(),
+                nurse.getLicenseNumber()
+        );
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public ProfessionalStatus getStatus() {
+        return status;
+    }
+
+    public NurseLicenseNumber getLicenseNumber() {
+        return licenseNumber;
+    }
+}
