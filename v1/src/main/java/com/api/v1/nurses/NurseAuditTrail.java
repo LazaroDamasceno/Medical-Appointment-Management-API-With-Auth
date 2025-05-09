96da100f06dff1da9490adac5b0a6b41aedd5cdf
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-09 02:53:56 -0300

    Created the entity NurseAuditTrail

---
+++ v1/src/main/java/com/api/v1/nurses/NurseAuditTrail.java
@@ -0,0 +1,22 @@
+package com.api.v1.nurses;
+
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record NurseAuditTrail(
+        @Id
+        String id,
+        Nurse nurse,
+        LocalDateTime createdAt
+) {
+
+    public static NurseAuditTrail of(Nurse nurse) {
+        return new NurseAuditTrail(
+                UUID.randomUUID().toString(),
+                nurse,
+                LocalDateTime.now()
+        );
+    }
+}
