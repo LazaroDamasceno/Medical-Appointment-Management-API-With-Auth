7afc66af109e0575b4a129fd06c8659fa0029f7b
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-11 17:41:57 -0300

    Add a static factory method to Person

--- v1/src/main/java/com/api/v1/people/domain/Person.java
+++ v1/src/main/java/com/api/v1/people/domain/Person.java
@@ -1,9 +1,11 @@
 package com.api.v1.people.domain;
 
+import com.api.v1.people.people.requests.PersonRegistrationDto;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Document
 public record Person(
@@ -17,4 +19,17 @@ public record Person(
         String email,
         LocalDateTime createdAt
 ) {
+
+    public static Person of(PersonRegistrationDto registrationDto) {
+        return new Person(
+                UUID.randomUUID().toString(),
+                registrationDto.firstName(),
+                registrationDto.middleName(),
+                registrationDto.lastName(),
+                registrationDto.sin(),
+                registrationDto.birthDate(),
+                registrationDto.email(),
+                LocalDateTime.now()
+        );
+    }
 }
