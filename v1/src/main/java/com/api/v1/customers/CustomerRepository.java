f6b1416812a4f5027f71e5a8047459a9ea8b7790
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-12 02:47:34 -0300

    Created the repository CustomerRepository

---
+++ v1/src/main/java/com/api/v1/customers/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.api.v1.customers;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    @Query("{ 'sin': ?0 }")
+    Optional<Customer> findBySin(String sin);
+
+    @Query("{ 'email': ?0 }")
+    Optional<Customer> findByEmail(String email);
+}
