2eb5ae6cae2ac8341a4fea6ba7adf2b28f89f48b
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-12 03:01:11 -0300

    Created the DTO CustomerResponseDto

---
+++ v1/src/main/java/com/api/v1/customers/dtos/CustomerResponseDto.java
@@ -0,0 +1,17 @@
+package com.api.v1.customers.dtos;
+
+import com.api.v1.customers.domain.Customer;
+import com.api.v1.people.FullNameFormatter;
+
+public record CustomerResponseDto(
+        String id,
+        String fullName
+) {
+
+    public static CustomerResponseDto from(Customer customer) {
+        return new CustomerResponseDto(
+                customer.getId(),
+                FullNameFormatter.format(customer.getPerson())
+        );
+    }
+}
