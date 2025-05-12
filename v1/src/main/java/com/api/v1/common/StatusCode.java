38e0ec514434c662fc9986918243f4a82bd8331f
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-12 16:34:27 -0300

    Recreated the class StatusCode

---
+++ v1/src/main/java/com/api/v1/common/StatusCode.java
@@ -0,0 +1,13 @@
+package com.api.v1.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class StatusCode {
+
+    public static final int OK = 200;
+    public static final int CREATED_ = 201;
+    public static final int NOT_FOUND = 404;
+    public static final int CONFLICT_409 = 409;
+
+}
