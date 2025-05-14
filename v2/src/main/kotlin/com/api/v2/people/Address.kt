f38ffaecf27ffefe4967d21576390be2aad2ac6d
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-14 01:53:49 -0300

    v2: Created the enum Address

---
+++ v2/src/main/kotlin/com/api/v2/people/Address.kt
@@ -0,0 +1,14 @@
+package com.api.v2.people
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class Address(
+    @NotBlank
+    val string: String,
+    @NotBlank
+    val city: String,
+    @NotBlank
+    @Size(min = 5, max = 5)
+    val zipcode: String
+)
