e8b2aef58acfa995bb048a99fe4177ad0e49d025
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-14 01:54:31 -0300

    v2: Created the DTO PersonUpdatingDto

---
+++ v2/src/main/kotlin/com/api/v2/people/PersonUpdatingDto.kt
@@ -0,0 +1,28 @@
+package com.api.v2.people
+
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import java.time.LocalDate
+
+data class PersonUpdatingDto(
+    @NotBlank
+    val firstName: String,
+    val middleName: String?,
+    @NotBlank
+    val lastName: String,
+    @NotNull
+    val birthDate: LocalDate,
+    @NotNull
+    val gender: Gender,
+    @Valid
+    val address: Address,
+    @NotBlank
+    @Email
+    var email: String,
+    @NotBlank
+    @Size(min = 10, max = 10)
+    var phoneNumber: String
+)
