033bbaf0418091748ff68e164ef82a78cebf06db
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-14 02:55:04 -0300

    v2: Created the entity CustomerAuditTrail

--- v2/src/main/kotlin/com/api/v2/customers/domain/CustomerRepository.kt
+++ v2/src/main/kotlin/com/api/v2/customers/domain/CustomerRepository.kt
@@ -1,5 +1,6 @@
-package com.api.v2.customers
+package com.api.v2.customers.domain
 
+import com.api.v2.customers.domain.Customer
 import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
