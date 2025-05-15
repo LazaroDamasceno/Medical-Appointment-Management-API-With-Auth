8144656ff042cd753842308974da3f5105a27cb8
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-14 22:46:05 -0300

    Created the entity DoctorAuditTrail

---
+++ v1/src/main/java/com/api/v1/doctors/DoctorAuditTrail.java
@@ -0,0 +1,23 @@
+package com.api.v1.doctors;
+
+import com.api.v1.people.domain.exposed.Person;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record DoctorAuditTrail(
+        @Id
+        String id,
+        Person person,
+        LocalDateTime createdAt
+) {
+
+    public static DoctorAuditTrail of(Person person) {
+        return new DoctorAuditTrail(
+                UUID.randomUUID().toString(),
+                person,
+                LocalDateTime.now()
+        );
+    }
+}
