6e9105dc379098438498b297e53a981666c82ac3
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-14 22:32:29 -0300

    Created the record LicenseNumber

---
+++ v1/src/main/java/com/api/v1/common/LicenseNumber.java
@@ -0,0 +1,14 @@
+package com.api.v1.common;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LicenseNumber(
+        @NotNull
+        HealthJob job,
+        @NotBlank
+        @Size(min = 10, max = 10)
+        String number
+) {
+}
