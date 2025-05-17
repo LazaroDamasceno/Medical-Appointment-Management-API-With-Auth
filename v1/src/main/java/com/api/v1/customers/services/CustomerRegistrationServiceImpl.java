6dd725cd759abb7bdde237ebbd47d8b0cacc422d
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-16 22:02:00 -0300

    Reimplemented the interface DoctorRegistrationService

--- v1/src/main/java/com/api/v1/customers/services/CustomerRegistrationServiceImpl.java
+++ v1/src/main/java/com/api/v1/customers/services/CustomerRegistrationServiceImpl.java
@@ -1,33 +1,51 @@
 package com.api.v1.customers.services;
 
+import com.api.v1.customers.domain.Customer;
 import com.api.v1.customers.domain.CustomerRepository;
 import com.api.v1.customers.response.CustomerResponseDto;
+import com.api.v1.people.domain.exposed.Person;
 import com.api.v1.people.exceptions.DuplicatedEmailException;
 import com.api.v1.people.exceptions.DuplicatedSINException;
 import com.api.v1.people.requests.PersonRegistrationDto;
+import com.api.v1.people.services.exposed.PersonRegistrationService;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
 
 @Service
 public class CustomerRegistrationServiceImpl implements CustomerRegistrationService {
 
-    private final CustomerRepository customerRepository;
+    private final CustomerRepository repository;
+    private final PersonRegistrationService personRegistrationService;
 
-    public CustomerRegistrationServiceImpl(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    public CustomerRegistrationServiceImpl(CustomerRepository repository,
+                                           PersonRegistrationService personRegistrationService
+    ) {
+        this.repository = repository;
+        this.personRegistrationService = personRegistrationService;
     }
 
     @Override
     public ResponseEntity<CustomerResponseDto> register(@Valid PersonRegistrationDto registrationDto) {
-        return null;
+        validate(registrationDto);
+        Person savedPerson = personRegistrationService.register(registrationDto);
+        Customer newCustomer = Customer.of(savedPerson);
+        Customer savedCustomer = repository.save(newCustomer);
+        CustomerResponseDto responseDto = savedCustomer.toDto();
+        return ResponseEntity
+                .created(URI.create("/api/v1/customers"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseDto);
     }
 
     private void validate(PersonRegistrationDto registrationDto) {
-        if (customerRepository.findBySIN(registrationDto.sin()).isPresent()) {
+        if (repository.findBySIN(registrationDto.sin()).isPresent()) {
             throw new DuplicatedSINException();
         }
-        if (customerRepository.findByEmail(registrationDto.email()).isPresent()) {
+        if (repository.findByEmail(registrationDto.email()).isPresent()) {
             throw new DuplicatedEmailException();
         }
     }
---
+++ v1/src/main/java/com/api/v1/doctors/services/DoctorRegistrationServiceImpl.java
@@ -0,0 +1,55 @@
+package com.api.v1.doctors.services;
+
+import com.api.v1.doctors.domain.DoctorRepository;
+import com.api.v1.doctors.domain.exposed.Doctor;
+import com.api.v1.doctors.exceptions.DuplicatedMedicalLicenseNumberException;
+import com.api.v1.doctors.requests.DoctorRegistrationDto;
+import com.api.v1.doctors.response.DoctorResponseDto;
+import com.api.v1.people.domain.exposed.Person;
+import com.api.v1.people.exceptions.DuplicatedSINException;
+import com.api.v1.people.services.exposed.PersonRegistrationService;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+
+@Service
+public class DoctorRegistrationServiceImpl implements DoctorRegistrationService {
+
+    private final DoctorRepository repository;
+    private final PersonRegistrationService personRegistrationService;
+
+    public DoctorRegistrationServiceImpl(DoctorRepository repository,
+                                         PersonRegistrationService personRegistrationService
+    ) {
+        this.repository = repository;
+        this.personRegistrationService = personRegistrationService;
+    }
+
+    @Override
+    public ResponseEntity<DoctorResponseDto> register(@Valid DoctorRegistrationDto registrationDto) {
+        validate(registrationDto);
+        Person savedPerson = personRegistrationService.register(registrationDto.person());
+        Doctor newDoctor = Doctor.of(savedPerson, registrationDto.licenseNumber());
+        Doctor savedDoctor = repository.save(newDoctor);
+        DoctorResponseDto responseDto = savedDoctor.toDto();
+        return ResponseEntity
+                .created(URI.create("/api/v1/doctors"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseDto);
+    }
+
+    private void validate(DoctorRegistrationDto registrationDto) {
+        if (repository.findByLicenseNumber(registrationDto.licenseNumber()).isPresent()) {
+            throw new DuplicatedMedicalLicenseNumberException(registrationDto.licenseNumber());
+        }
+        if (repository.findBySIN(registrationDto.person().sin()).isPresent()) {
+            throw new DuplicatedSINException();
+        }
+        if (repository.findByEmail(registrationDto.person().email()).isPresent()) {
+            throw new DuplicatedSINException();
+        }
+    }
+}
