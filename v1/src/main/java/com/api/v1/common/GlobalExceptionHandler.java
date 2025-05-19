a3959c9dfb899572d7939803682c8fd4c7152e06
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-19 02:28:58 -0300

    Created the class GlobalExceptionHandler and exposed all the exceptions

---
+++ v1/src/main/java/com/api/v1/common/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.api.v1.common;
+
+import com.api.v1.customers.exceptions.*;
+import com.api.v1.doctors.exceptions.*;
+import com.api.v1.people.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicatedSINException.class)
+    public ResponseEntity<String> handleException(DuplicatedSINException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicatedEmailException.class)
+    public ResponseEntity<String> handleException(DuplicatedEmailException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<String> handleException(CustomerNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ActiveDoctorException.class)
+    public ResponseEntity<String> handleException(ActiveDoctorException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DoctorNotFoundException.class)
+    public ResponseEntity<String> handleException(DoctorNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicatedMedicalLicenseNumberException.class)
+    public ResponseEntity<String> handleException(DuplicatedMedicalLicenseNumberException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(TerminatedDoctorException.class)
+    public ResponseEntity<String> handleException(TerminatedDoctorException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+}
