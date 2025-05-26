e1e7e8ca62cf4c7062d5110145134fbeab180db9
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-26 20:13:22 -0300

    v2: created the entity Doctor.kt

---
+++ v2/src/main/kotlin/com/api/v2/doctors/Doctor.kt
@@ -0,0 +1,33 @@
+package com.api.v2.doctors
+
+import com.api.v2.common.ProfessionalStatus
+import com.api.v2.people.domain.exposed.Person
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document
+class Doctor private constructor(
+    var licenseNumber: String,
+    var person: Person
+) {
+
+    var status: ProfessionalStatus = ProfessionalStatus.ACTIVE
+    val createdAt: LocalDateTime = LocalDateTime.now()
+    var terminatedAt: LocalDateTime? = null
+
+    companion object {
+        fun of(licenseNumber: String, person: Person): Doctor {
+            return Doctor(licenseNumber, person)
+        }
+    }
+
+    fun markAsTerminated() {
+        status = ProfessionalStatus.TERMINATED
+        terminatedAt = LocalDateTime.now()
+    }
+
+    fun markAsRehired() {
+        status = ProfessionalStatus.ACTIVE
+        terminatedAt = null
+    }
+}
