0defce1cec156ab7aa61a70ae13a85fa0b725d4e
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-27 21:15:18 -0300

    v2: created the service DoctorRegistrationService

---
+++ v2/src/main/kotlin/com/api/v2/doctors/services/DoctorRegistrationService.kt
@@ -0,0 +1,9 @@
+package com.api.v2.doctors.services
+
+import com.api.v2.doctors.requests.DoctorRegistrationDTO
+import com.api.v2.doctors.responses.DoctorResponseDTO
+import org.springframework.http.ResponseEntity
+
+interface DoctorRegistrationService {
+    fun register(registrationDTO: DoctorRegistrationDTO): ResponseEntity<DoctorResponseDTO>
+}
---
+++ v2/src/main/kotlin/com/api/v2/doctors/services/DoctorRegistrationServiceImpl.kt
@@ -0,0 +1,52 @@
+package com.api.v2.doctors.services
+
+import com.api.v2.common.DuplicatedLicenseNumberException
+import com.api.v2.doctors.domain.DoctorCrudRepository
+import com.api.v2.doctors.domain.exposed.Doctor
+import com.api.v2.doctors.requests.DoctorRegistrationDTO
+import com.api.v2.doctors.responses.DoctorResponseDTO
+import com.api.v2.doctors.utils.exposed.toDTO
+import com.api.v2.people.exceptions.DuplicatedEmailException
+import com.api.v2.people.exceptions.DuplicatedSINException
+import com.api.v2.people.services.exposed.PersonRegistrationService
+import jakarta.validation.Valid
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import java.net.URI
+
+@Service
+class DoctorRegistrationServiceImpl: DoctorRegistrationService {
+
+    private val personRegistrationService: PersonRegistrationService
+    private val crudRepository: DoctorCrudRepository
+
+    constructor(personRegistrationService: PersonRegistrationService, crudRepository: DoctorCrudRepository) {
+        this.personRegistrationService = personRegistrationService
+        this.crudRepository = crudRepository
+    }
+
+    override fun register(registrationDTO: @Valid DoctorRegistrationDTO): ResponseEntity<DoctorResponseDTO> {
+        validate(registrationDTO)
+        val savedPerson = personRegistrationService.register(registrationDTO.person)
+        val newDoctor = Doctor.of(registrationDTO.licenseNumber, savedPerson)
+        val savedDoctor = crudRepository.save(newDoctor)
+        val dto = savedDoctor.toDTO()
+        return ResponseEntity
+            .created(URI.create("/api/v2/doctors"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(dto)
+    }
+
+    fun validate(registrationDTO: DoctorRegistrationDTO) {
+        if (crudRepository.findByLicenseNumber(registrationDTO.licenseNumber) != null) {
+            throw DuplicatedLicenseNumberException()
+        }
+        if (crudRepository.findBySIN(registrationDTO.person.sin) != null) {
+            throw DuplicatedSINException()
+        }
+        if (crudRepository.findByEmail(registrationDTO.person.email) != null) {
+            throw DuplicatedEmailException()
+        }
+    }
+}
