de951475eb3e48103ce1dbb1aca49503bb2c34b2
lazarodamasceno <lazarolawrence2@gmail.com>
2025-05-29 19:10:33 -0300

    Deleted the folder nurses

--- v1/src/main/java/com/api/v1/nurses/services/NurseRegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.nurses.services;
-
-import com.api.v1.nurses.requests.NurseRegistrationDTO;
-import com.api.v1.nurses.responses.NurseResponseDTO;
-import org.springframework.http.ResponseEntity;
-
-public interface NurseRegistrationService {
-    ResponseEntity<NurseResponseDTO> register(NurseRegistrationDTO registrationDTO);
-}
--- v1/src/main/java/com/api/v1/nurses/utils/exposed/NurseFinder.java
+++
@@ -1,7 +0,0 @@
-package com.api.v1.nurses.utils.exposed;
-
-import com.api.v1.nurses.domain.exposed.Nurse;
-
-public interface NurseFinder {
-    Nurse findByLicenseNumber(String licenseNumber);
-}
