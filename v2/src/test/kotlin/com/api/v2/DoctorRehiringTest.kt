f4acf88edeabf9816a9acecd7e0d348dc3cf8613
lazarodamasceno <lazarolawrence2@gmail.com>
2025-06-01 03:02:04 -0300

    v2: created the testing classes DoctorRehiringTest and DoctorTerminationTest

---
+++ v2/src/test/kotlin/com/api/v2/DoctorRehiringTest.kt
@@ -0,0 +1,60 @@
+package com.api.v2
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.UUID
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class DoctorRehiringTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    @Order(1)
+    @Test
+    fun `should return no content when successful`() {
+        val licenseNumber = ""
+        mockMvc.perform(
+            patch("/api/v2/doctors/$licenseNumber/rehiring")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated)
+    }
+
+    @Order(2)
+    @Test
+    fun `should return conflict when doctor is terminated`() {
+        val licenseNumber = ""
+        mockMvc.perform(
+            patch("/api/v2/doctors/$licenseNumber/rehiring")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isConflict)
+    }
+
+    @Order(3)
+    @Test
+    fun `should return not found when doctor was not found`() {
+        val licenseNumber = UUID.randomUUID()
+            .toString()
+            .replace("-", "")
+            .substring(0, 10)
+        mockMvc.perform(
+            patch("/api/v2/doctors/$licenseNumber/rehiring")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound)
+    }
+}
