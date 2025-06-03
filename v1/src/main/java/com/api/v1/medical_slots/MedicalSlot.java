fc90df094d969124fda684ec3a11faef0ba285ba
lazarodamasceno <lazarolawrence2@gmail.com>
2025-06-03 17:13:31 -0300

    Created the entity MedicalSlot and the enum MedicalSlotStatus

---
+++ v1/src/main/java/com/api/v1/medical_slots/MedicalSlot.java
@@ -0,0 +1,46 @@
+package com.api.v1.medical_slots;
+
+import com.api.v1.doctors.Doctor;
+import com.api.v1.medical_slots.enums.MedicalSlotStatus;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document(collection = "MedicalSlots")
+public class MedicalSlot {
+
+    @Id
+    private String id;
+    private Doctor doctor;
+    private MedicalSlotStatus status;
+    private LocalDateTime availableAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime cancelledAt;
+    private LocalDateTime completedAt;
+
+    private MedicalSlot() {}
+
+    private MedicalSlot(Doctor doctor, LocalDateTime availableAt) {
+        this.id = UUID.randomUUID().toString();
+        this.status = MedicalSlotStatus.ACTIVE;
+        this.doctor = doctor;
+        this.availableAt = availableAt;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static MedicalSlot of(Doctor doctor, LocalDateTime availableAt) {
+        return new MedicalSlot(doctor, availableAt);
+    }
+
+    public void markAsCancelled() {
+        this.status = MedicalSlotStatus.CANCELLED;
+        this.cancelledAt = LocalDateTime.now();
+    }
+
+    public void markAsCompleted() {
+        this.status = MedicalSlotStatus.COMPLETED;
+        this.completedAt = LocalDateTime.now();
+    }
+}
---
+++ v1/src/main/java/com/api/v1/medical_slots/enums/MedicalSlotStatus.java
@@ -0,0 +1,7 @@
+package com.api.v1.medical_slots.enums;
+
+public enum MedicalSlotStatus {
+    ACTIVE,
+    CANCELLED,
+    COMPLETED
+}
