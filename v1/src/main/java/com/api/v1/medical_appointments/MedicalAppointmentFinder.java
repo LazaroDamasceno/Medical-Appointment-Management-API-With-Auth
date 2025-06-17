cdb58311afd0cb0f51d3a96271a88bdcae51ed70
lazarodamasceno <lazarolawrence2@gmail.com>
2025-06-17 02:26:19 -0300

    Added to the utility class MedicalAppointmentFinder the method MedicalAppointmentFinder

--- v1/src/main/java/com/api/v1/medical_appointments/MedicalAppointmentFinder.java
+++ v1/src/main/java/com/api/v1/medical_appointments/MedicalAppointmentFinder.java
@@ -1,6 +1,10 @@
 package com.api.v1.medical_appointments;
 
+import com.api.v1.customers.Customer;
+import com.api.v1.doctors.Doctor;
+
 public interface MedicalAppointmentFinder {
     MedicalAppointment findById(String id);
-    MedicalAppointment findByIdAndCustomer(String id, String customerId);
+    MedicalAppointment findByIdAndCustomer(String id, Customer customer);
+    MedicalAppointment findByIdAndDoctor(String id, Doctor doctor);
 }
--- v1/src/main/java/com/api/v1/medical_appointments/utils/MedicalAppointmentFinderImpl.java
+++ v1/src/main/java/com/api/v1/medical_appointments/utils/MedicalAppointmentFinderImpl.java
@@ -2,12 +2,13 @@ package com.api.v1.medical_appointments.utils;
 
 import com.api.v1.common.ObjectId;
 import com.api.v1.customers.Customer;
-import com.api.v1.customers.CustomerFinder;
+import com.api.v1.doctors.Doctor;
 import com.api.v1.medical_appointments.MedicalAppointment;
 import com.api.v1.medical_appointments.MedicalAppointmentFinder;
 import com.api.v1.medical_appointments.domain.MedicalAppointmentCrudRepository;
 import com.api.v1.medical_appointments.exceptions.InaccessibleMedicalAppointmentException;
 import com.api.v1.medical_appointments.exceptions.MedicalAppointmentNotFoundException;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,14 +17,9 @@ import java.util.Optional;
 public class MedicalAppointmentFinderImpl implements MedicalAppointmentFinder {
 
     private final MedicalAppointmentCrudRepository crudRepository;
-    private final CustomerFinder customerFinder;
 
-    public MedicalAppointmentFinderImpl(
-            MedicalAppointmentCrudRepository crudRepository,
-            CustomerFinder customerFinder
-    ) {
+    public MedicalAppointmentFinderImpl(MedicalAppointmentCrudRepository crudRepository) {
         this.crudRepository = crudRepository;
-        this.customerFinder = customerFinder;
     }
 
     @Override
@@ -33,13 +29,24 @@ public class MedicalAppointmentFinderImpl implements MedicalAppointmentFinder {
     }
 
     @Override
-    public MedicalAppointment findByIdAndCustomer(@ObjectId String id, @ObjectId String customerId) {
-        Customer foundCustomer = customerFinder.findById(customerId);
+    public MedicalAppointment findByIdAndCustomer(@ObjectId String id, @NotNull Customer customer) {
         MedicalAppointment foundAppointment = findById(id);
-        if (foundAppointment.customer().id().equals(foundCustomer.id())) {
+        if (foundAppointment.customer().id().equals(customer.id())) {
             String message = """
-                    Medical appointment whose id is %s cannot be accessed by customer who id is %s.
-                    """.formatted(id, customerId);
+                    Medical appointment whose id is %s cannot be accessed by customer whose id is %s.
+                    """.formatted(id, customer.id());
+            throw new InaccessibleMedicalAppointmentException(message);
+        }
+        return foundAppointment;
+    }
+
+    @Override
+    public MedicalAppointment findByIdAndDoctor(@ObjectId String id, @NotNull Doctor doctor) {
+        MedicalAppointment foundAppointment = findById(id);
+        if (foundAppointment.doctor().id().equals(doctor.id())) {
+            String message = """
+                    Medical appointment whose id is %s cannot be accessed by doctor whose medical license number is %s.
+                    """.formatted(id, doctor.licenseNumber());
             throw new InaccessibleMedicalAppointmentException(message);
         }
         return foundAppointment;
