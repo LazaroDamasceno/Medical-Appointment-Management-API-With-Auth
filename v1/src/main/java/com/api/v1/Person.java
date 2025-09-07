33ee799cebccef511ff00e749a92167865ae8e87
lazarodamasceno <lazarolawrence2@gmail.com>
2025-09-07 17:53:09 -0300

    Created the class Person

---
+++ v1/src/main/java/com/api/v1/Person.java
@@ -0,0 +1,19 @@
+package com.api.v1;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class Person {
+
+    private String id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private LocalDate birthDate;
+    // Single Identification Number
+    private String sin;
+    private String email;
+    private String phoneNumber;
+}
